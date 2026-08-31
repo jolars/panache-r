96c29080200c56bc919fa837fcc0f423d986a96a
Johan Larsson <johan@jolars.co>
2026-08-31 16:19:22 +0200

    chore: setup devenv

---
+++ devenv.nix
@@ -0,0 +1,46 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  packages = [
+    pkgs.bashInteractive
+    pkgs.checkbashisms
+    pkgs.cargo-audit
+    pkgs.cargo-deny
+    pkgs.cargo-msrv
+    pkgs.go-task
+    pkgs.llvmPackages.bintools
+  ];
+
+  languages = {
+    rust = {
+      enable = true;
+      toolchainFile = ./src/rust/rust-toolchain.toml;
+    };
+
+    r = {
+      enable = true;
+
+      package = (
+        pkgs.rWrapper.override {
+          packages = with pkgs.rPackages; [
+            covr
+            devtools
+            knitr
+            lattice
+            rextendr
+            rmarkdown
+            spelling
+            testthat
+            urlchecker
+            remotes
+            rstudioapi
+            testthat
+          ];
+        }
+      );
+    };
+  };
+}
